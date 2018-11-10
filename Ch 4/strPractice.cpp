07d94f017ed2712ebaf2da281549fc3b0d39a7a4
Sam <vernaza1992@gmail.com>
2018-11-10 08:05:32 -0800

    Upadate

--- Ch 4/strPractice.cpp
+++ Ch 4/strPractice.cpp
@@ -1,8 +1,9 @@
 //Samuel Vernaza
-//Little program to demonstrate major differences between the string library and arrays
+// This program demonstrates the differences between using char and string
 
 #include <iostream>
 #include <string>
+#include <cstring>
 
 using namespace std;
 
@@ -17,35 +18,43 @@ int main()
 	string str3;
 
 	//assignment example
-	cout << "str2 is: " << str2 << endl;
-	cout << "Using string object assignment copying str2 to str1: " << endl;
+	cout << "\nstr2 is: " << str2 << endl;
+	cout << "\nUsing string object assignment copying str2 to str1: " << endl;
 	str1 = str2; //string assignment
-	cout << "Str1 is now: " << str1 << endl;
+	cout << "\nStr1 is now: " << str1 << endl;
 
 	//use of getline()
-	cout << "Using getline to get a multi word user input\n";
-	cout << "Enter your full name: \n";
+	cout << "\nUsing getline to get a multi word user input\n";
+	cout << "\nEnter your full name: \n";
 	cin.getline(arr1, arrSize);
-	cout << "You've entered: \n";
-	cout << arr1;
+	cout << "\nYou've entered: " << arr1 << endl;
 
 	//use of strcpy and overloaded assignment operator
 	cout << "\nCopying contents of arr2 into arr3\n";
 	strcpy(arr3, arr2);
-	cout << "arr3 is now: " << arr3 << endl;
+	cout << "\narr3 is now: " << arr3 << endl;
 
+	//copying string to another string using assingment operator
 	cout << "\nCopying one string to another \n";
-	str3 = str2;
+	str3 += str2;
 	cout << "\nStr 3 is now: " << str3 << endl;
 
+	//concatenates str2 to str1 (no spaces)
 	cout << "\nNow, concatenation! " << endl;
 	str1 += str2;
-	cout << "Str1 is now: " << str1 << endl;
+	cout << "\nStr1 is now: " << str1 << endl;
 
+	//concatenates an arr2 to arr1 (no spaces)
 	strcat(arr1, arr2);
 	cout << "\narr1 is now " << arr1 << endl;
-	cout << "Done! " << endl;
 
-	//example of output;
+	//get the length of an str
+	int lenStr = str1.size();
+	int lenChar = strlen(arr1);
+	cout << "\nLength of str1: " << lenStr << endl;
+	cout << "\nLength of arr1: " << lenChar << endl;
+
+	cout << "\nEnd..." << endl;
+
 	return 0;
 }
